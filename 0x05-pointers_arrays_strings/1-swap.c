60557d813fc876a6d0d5189b6f0432a6f571f790
nyakiomaina <carolinenyakio21@gmail.com>
2022-12-20 02:01:50 +0300

    challenge 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *swap_int-swaps value of two integers
+ *@a:1st operand
+ *@b:2nd operand
+ *Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+int temp;
+
+temp = *a;
+*a = *b;
+*b = temp;
+}
